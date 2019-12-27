0ccc6d198668b577df567c38e5c8763bde0da1e2
liongkj <fantasticbabii@gmail.com>
2019-12-27 16:49:06 +0800

    fix sliver app bar

--- lib/screens/tabs/overview/pages/place_detail_page.dart
+++ lib/screens/tabs/overview/pages/place_detail_page.dart
@@ -5,11 +5,15 @@ import 'package:jom_malaysia/core/mvp/base_page_state.dart';
 import 'package:jom_malaysia/core/res/colors.dart';
 import 'package:jom_malaysia/core/res/resources.dart';
 import 'package:jom_malaysia/screens/tabs/overview/models/listing_model.dart';
+import 'package:jom_malaysia/screens/tabs/overview/pages/overview_page.dart';
 import 'package:jom_malaysia/screens/tabs/overview/presenter/place_detail_page_presenter.dart';
 import 'package:jom_malaysia/screens/tabs/overview/providers/place_detail_provider.dart';
 import 'package:jom_malaysia/setting/routers/fluro_navigator.dart';
+import 'package:jom_malaysia/util/image_utils.dart';
+import 'package:jom_malaysia/widgets/load_image.dart';
 import 'package:jom_malaysia/widgets/my_card.dart';
 import 'package:jom_malaysia/widgets/my_flexible_space_bar.dart';
+import 'package:jom_malaysia/widgets/my_rating.dart';
 import 'package:jom_malaysia/widgets/state_layout.dart';
 import 'package:provider/provider.dart';
 
@@ -23,6 +27,8 @@ class PlaceDetailPage extends StatefulWidget {
   PlaceDetailPageState createState() => PlaceDetailPageState();
 }
 
+const kExpandedHeight = 250.0;
+
 class PlaceDetailPageState
     extends BasePageState<PlaceDetailPage, PlaceDetailPagePresenter> {
   bool isDark = false;
@@ -34,10 +40,13 @@ class PlaceDetailPageState
     provider.setPlace(place);
   }
 
+  ScrollController _scrollController;
+
   @override
   void initState() {
     super.initState();
     provider.setStateTypeNotNotify(StateType.empty);
+    _scrollController = ScrollController()..addListener(() => setState(() {}));
   }
 
   @override
@@ -50,6 +59,11 @@ class PlaceDetailPageState
     super.dispose();
   }
 
+  bool get _showTitle {
+    return _scrollController.hasClients &&
+        _scrollController.offset > kExpandedHeight - kToolbarHeight;
+  }
+
   @override
   Widget build(BuildContext context) {
     isDark = ThemeUtils.isDark(context);
@@ -58,10 +72,13 @@ class PlaceDetailPageState
         child: Consumer<PlaceDetailProvider>(builder: (_, detail, __) {
           final place = detail.place;
           return Scaffold(
-            body: CustomScrollView(
-              key: const Key('place_detail'),
-              physics: ClampingScrollPhysics(),
-              slivers: _sliverBuilder(place),
+            body: SafeArea(
+              child: CustomScrollView(
+                controller: _scrollController,
+                key: const Key('place_detail'),
+                physics: BouncingScrollPhysics(),
+                slivers: _sliverBuilder(place),
+              ),
             ),
           );
         }));
@@ -72,29 +89,95 @@ class PlaceDetailPageState
     images.addAll(place.listingImages.ads.map((x) => x.url).toList());
     return <Widget>[
       SliverAppBar(
-        brightness: Brightness.dark,
-        backgroundColor: Colors.transparent,
+        // brightness: Brightness.dark,
+        backgroundColor: ThemeUtils.getBackgroundColor(context),
         titleSpacing: 0.0,
-        centerTitle: true,
-        expandedHeight: 250.0,
-        floating: false, // 不随着滑动隐藏标题
-        pinned: false, // 固定在顶部
-        flexibleSpace: MyFlexibleSpaceBar(
-          titlePadding:
-              const EdgeInsetsDirectional.only(start: 16.0, bottom: 14.0),
-          collapseMode: CollapseMode.pin,
-          background: _CoverPhotos(images),
-          title: Text(
-            '${place.category.getCategory()} detail',
-            style: TextStyle(color: ThemeUtils.getIconColor(context)),
-          ),
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: _showTitle ? Colors.black : ThemeUtils.getIconColor(context),
         ),
+        centerTitle: true,
+        title: _showTitle
+            ? Text(
+                '${place.category.getCategory()} detail',
+                style: Theme.of(context).textTheme.title,
+              )
+            : null,
+        expandedHeight: kExpandedHeight,
+        floating: false, // 不随着滑动隐藏标题
+        pinned: true, // 固定在顶部
+        flexibleSpace: _showTitle
+            ? null
+            : FlexibleSpaceBar(
+                titlePadding:
+                    const EdgeInsetsDirectional.only(start: 16.0, bottom: 14.0),
+                collapseMode: CollapseMode.pin,
+                background: _CoverPhotos(images),
+                centerTitle: true,
+              ),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.favorite_border),
+            icon: Icon(
+              Icons.favorite_border,
+              color:
+                  _showTitle ? Colors.black : ThemeUtils.getIconColor(context),
+            ),
             onPressed: () {},
           )
         ],
+      ),
+      SliverPersistentHeader(
+        delegate: SliverAppBarDelegate(
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: MyCard(
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        child: LoadImage(
+                          place.listingImages.coverPhoto.url,
+                          width: 100.0,
+                          height: 100.0,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Gaps.hGap10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Text(
+                              place.listingName,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Gaps.vGap8,
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Rating(rating: 4),
+                                ),
+                                Text(
+                                  place.tags[0],
+                                  style: TextStyle(
+                                      fontSize: Dimens.font_sp12,
+                                      color: Theme.of(context).errorColor),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            120),
       ),
       SliverToBoxAdapter(
         child: Padding(
@@ -135,7 +218,7 @@ class _CoverPhotos extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return Image.network(
           swiperImage[index],
-          fit: BoxFit.fill,
+          fit: BoxFit.cover,
         );
       },
       itemCount: count,
